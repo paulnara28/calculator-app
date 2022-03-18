fd64b51c910898c82e10854fb153b7c0397f2fc2
paul <paulnara@students.amikom.ac.id>
2022-03-18 21:13:37 +0800

    melengkapi method main

--- calculator.cs
+++ calculator.cs
@@ -2,6 +2,14 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		Console.Writeline("Hasil Penambahan: {0} + {1}", a, b, Penambahan(a, b));
+		Console.Writeline("Hasil Pengurangan: {0} + {1}", a, b, Pengurangan(a, b));
+		
+		Console.Writeline("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
 	}
 	
 	static int Penambahan(int a, int b)
